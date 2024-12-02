3ab44b4aa91bc8e28d257eab8b079438be5a3cec
adrian <adrian@LAPTOP-Q2I2QCRG>
2024-12-03 00:58:20 +0800

    added a press function on all buttons

--- lib/NavigationPages/settings_page.dart
+++ lib/NavigationPages/settings_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:pasada_driver_side/NavigationPages/main_page.dart';
+import 'package:pasada_driver_side/NavigationPages/notification_page.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({super.key});
@@ -12,15 +14,29 @@ class SettingsPage extends StatelessWidget {
           left: MediaQuery.of(context).size.width * 0.05,
           right: MediaQuery.of(context).size.width * 0.05, // 5% of screen width
         ),
-        child: const Column(children: [
-          SettingsButtons('Notification'),
-          SizedBox(height: 20),
-          RouteSuggestionToggleButton('Alternative Route Suggestion'),
-          SizedBox(height: 20),
-          SettingsButtons('Preference'),
-          SizedBox(height: 20),
-          SettingsButtons('Support'),
-          SizedBox(height: 20),
+        child: Column(children: [
+          SettingsButtons(
+            'Notification',
+            onPressed: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const NotificationPage()));
+            },
+          ),
+          const SizedBox(height: 20),
+          const RouteSuggestionToggleButton('Alternative Route Suggestion'),
+          const SizedBox(height: 20),
+          SettingsButtons(
+            'Preference',
+            onPressed: () {},
+          ),
+          const SizedBox(height: 20),
+          SettingsButtons(
+            'Support',
+            onPressed: () {},
+          ),
+          const SizedBox(height: 20),
         ]),
       ),
     );
@@ -29,27 +45,31 @@ class SettingsPage extends StatelessWidget {
 
 class SettingsButtons extends StatelessWidget {
   final String buttonName;
-  const SettingsButtons(this.buttonName, {super.key});
+  final VoidCallback onPressed;
+  const SettingsButtons(this.buttonName, {super.key, required this.onPressed});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 50,
-      decoration: BoxDecoration(
-        border: Border.all(
-          color: const Color(0xFF5F3FC4),
-          width: 2.0,
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        height: 50,
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: const Color(0xFF5F3FC4),
+            width: 2.0,
+          ),
+          borderRadius: BorderRadius.circular(30.0),
         ),
-        borderRadius: BorderRadius.circular(30.0),
-      ),
-      alignment: Alignment.centerLeft,
-      padding: const EdgeInsets.only(left: 20),
-      child: Text(
-        buttonName,
-        style: const TextStyle(
-          fontFamily: 'Inter',
-          fontSize: 15,
-          fontWeight: FontWeight.w500,
+        alignment: Alignment.centerLeft,
+        padding: const EdgeInsets.only(left: 20),
+        child: Text(
+          buttonName,
+          style: const TextStyle(
+            fontFamily: 'Inter',
+            fontSize: 15,
+            fontWeight: FontWeight.w500,
+          ),
         ),
       ),
     );
